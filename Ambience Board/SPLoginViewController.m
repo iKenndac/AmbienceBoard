f14685b2ec1bfbd1658c730bdb92ae021b64e844
Daniel Kennett <dan@spotify.com>
2012-02-12 12:07:50 -0800

    Rotating login screen

--- Ambience Board/SPLoginViewController.m
+++ Ambience Board/SPLoginViewController.m
@@ -157,7 +157,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 
